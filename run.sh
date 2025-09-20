a9f2026ceb2f041157a4cba15287577d09bdbc40
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2025-09-19 22:59:30 -0600

    add wrapper script to run the Rust release binary

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# set -x
+set -e
+
+readonly script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+readonly bin_file="my-arbolitos"
+readonly error_bin_file_not_found=80
+
+# echo "El script está en: $script_dir"
+if [[ ! -f "${script_dir}/target/release/${bin_file}" ]]; then
+    echo "error: binary file ${bin_file} not found" >&2
+    exit ${error_bin_file_not_found}
+fi
+
+"${script_dir}/target/release/${bin_file}" $*
+
+exit 
